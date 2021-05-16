fe057ca0378353ac2c90ee88042e3562d338c290
Anthony Mai <anthonymai14@gmail.com>
2021-05-15 22:01:24 -0700

    Setup Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+class Calculator {
+    public double add(Double a, Double b) {
+
+    }
+    
+    public double subtract(Double a, Double b) {
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
