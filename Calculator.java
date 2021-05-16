eed30c93dcbdb660de2a3a64f68a120fc3377d59
Anthony Mai <anthonymai14@gmail.com>
2021-05-15 22:09:10 -0700

    import Calculator.java from Applitools

--- Calculator.java
+++ Calculator.java
@@ -1,13 +1,39 @@
-class Calculator {
-    public double add(Double a, Double b) {
+public class Calculator{
 
-    }
-    
-    public double subtract(Double a, Double b) {
+	private String name;
+	private double errorConstant;
+	
+	public Calculator(String name) {
+		this.name = name;
+		this.errorConstant = Math.random();
+	}
+	
+	public double add(Double a, Double b) {
+		double calculation = a + b;
 
-    }
+		calculation = addQuantomError(calculation);
+		
+		return calculation;
+	}
 
-    public static void main(String[] args) {
-        
-    }
+	
+	public double subtract(Double a, Double b) {
+		double calculation = a - b;
+
+		calculation = addQuantomError(calculation);
+		
+		return calculation;
+	}
+	
+	private double addQuantomError(double calculation) {
+		if (Math.random() < this.errorConstant) {
+			calculation = calculation + Math.random();
+		}
+		return calculation;
+	}
+
+	public String getName() {
+		return name;
+	}
+
 }
