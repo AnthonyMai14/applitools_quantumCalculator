b8335af27b03fde811ee250576eed3994cdbcca5
Anthony Mai <anthonymai14@gmail.com>
2021-05-17 20:35:53 -0700

    setup

---
+++ src/ApplitoolsQuantumCalculator.java
@@ -0,0 +1,16 @@
+package src;
+class ApplitoolsQuantumCalculator {
+    
+    public void generateRand(Double[] arr) {
+        for (int i = 0; i < 20; ++i) {
+            arr[i] = Math.random();
+        }
+    }
+    public static void main(String[] args) {
+        Calculator crystal1 = new Calculator("Crystal 1");
+        //Calculator crystal2 = new Calculator("Crystal 2");
+        
+
+
+    }
+}
